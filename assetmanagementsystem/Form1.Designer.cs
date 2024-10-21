04ad7693c3f9c43df508544794a9a7ec875deba0
Monalisuzkz <124888741+Monalisuzkz@users.noreply.github.com>
2024-10-21 23:44:41 +0800

    Add project files.

---
+++ assetmanagementsystem/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace assetmanagementsystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gunaLabel1 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel2 = new Guna.UI.WinForms.GunaLabel();
+            this.tb_username = new Guna.UI2.WinForms.Guna2TextBox();
+            this.gunaLabel3 = new Guna.UI.WinForms.GunaLabel();
+            this.tb_password = new Guna.UI2.WinForms.Guna2TextBox();
+            this.btn_login = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2ImageButton1 = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.SuspendLayout();
+            // 
+            // gunaLabel1
+            // 
+            this.gunaLabel1.AutoSize = true;
+            this.gunaLabel1.Font = new System.Drawing.Font("Cascadia Mono", 18F, System.Drawing.FontStyle.Bold);
+            this.gunaLabel1.Location = new System.Drawing.Point(315, 55);
+            this.gunaLabel1.Name = "gunaLabel1";
+            this.gunaLabel1.Size = new System.Drawing.Size(125, 47);
+            this.gunaLabel1.TabIndex = 0;
+            this.gunaLabel1.Text = "LOGIN";
+            // 
+            // gunaLabel2
+            // 
+            this.gunaLabel2.AutoSize = true;
+            this.gunaLabel2.Font = new System.Drawing.Font("Cascadia Mono", 10F);
+            this.gunaLabel2.Location = new System.Drawing.Point(165, 127);
+            this.gunaLabel2.Name = "gunaLabel2";
+            this.gunaLabel2.Size = new System.Drawing.Size(120, 27);
+            this.gunaLabel2.TabIndex = 1;
+            this.gunaLabel2.Text = "Username:";
+            // 
+            // tb_username
+            // 
+            this.tb_username.BorderRadius = 10;
+            this.tb_username.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tb_username.DefaultText = "";
+            this.tb_username.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.tb_username.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.tb_username.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.tb_username.DisabledState.Parent = this.tb_username;
+            this.tb_username.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.tb_username.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.tb_username.FocusedState.Parent = this.tb_username;
+            this.tb_username.Font = new System.Drawing.Font("Cascadia Mono", 9F);
+            this.tb_username.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.tb_username.HoverState.Parent = this.tb_username;
+            this.tb_username.Location = new System.Drawing.Point(169, 160);
+            this.tb_username.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.tb_username.Name = "tb_username";
+            this.tb_username.PasswordChar = '\0';
+            this.tb_username.PlaceholderText = "";
+            this.tb_username.SelectedText = "";
+            this.tb_username.ShadowDecoration.Parent = this.tb_username;
+            this.tb_username.Size = new System.Drawing.Size(410, 54);
+            this.tb_username.TabIndex = 2;
+            // 
+            // gunaLabel3
+            // 
+            this.gunaLabel3.AutoSize = true;
+            this.gunaLabel3.Font = new System.Drawing.Font("Cascadia Mono", 10F);
+            this.gunaLabel3.Location = new System.Drawing.Point(164, 232);
+            this.gunaLabel3.Name = "gunaLabel3";
+            this.gunaLabel3.Size = new System.Drawing.Size(120, 27);
+            this.gunaLabel3.TabIndex = 3;
+            this.gunaLabel3.Text = "Password:";
+            // 
+            // tb_password
+            // 
+            this.tb_password.BorderRadius = 10;
+            this.tb_password.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tb_password.DefaultText = "";
+            this.tb_password.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.tb_password.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.tb_password.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.tb_password.DisabledState.Parent = this.tb_password;
+            this.tb_password.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.tb_password.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.tb_password.FocusedState.Parent = this.tb_password;
+            this.tb_password.Font = new System.Drawing.Font("Cascadia Mono", 9F);
+            this.tb_password.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.tb_password.HoverState.Parent = this.tb_password;
+            this.tb_password.Location = new System.Drawing.Point(169, 265);
+            this.tb_password.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.PasswordChar = '\0';
+            this.tb_password.PlaceholderText = "";
+            this.tb_password.SelectedText = "";
+            this.tb_password.ShadowDecoration.Parent = this.tb_password;
+            this.tb_password.Size = new System.Drawing.Size(410, 54);
+            this.tb_password.TabIndex = 4;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Animated = true;
+            this.btn_login.BorderRadius = 10;
+            this.btn_login.CheckedState.Parent = this.btn_login;
+            this.btn_login.CustomImages.Parent = this.btn_login;
+            this.btn_login.Font = new System.Drawing.Font("Cascadia Mono", 10F);
+            this.btn_login.ForeColor = System.Drawing.Color.White;
+            this.btn_login.HoverState.Parent = this.btn_login;
+            this.btn_login.Location = new System.Drawing.Point(170, 355);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.PressedColor = System.Drawing.Color.Azure;
+            this.btn_login.ShadowDecoration.Parent = this.btn_login;
+            this.btn_login.Size = new System.Drawing.Size(409, 50);
+            this.btn_login.TabIndex = 5;
+            this.btn_login.Text = "LOGIN";
+            // 
+            // guna2ImageButton1
+            // 
+            this.guna2ImageButton1.CheckedState.Parent = this.guna2ImageButton1;
+            this.guna2ImageButton1.HoverState.Parent = this.guna2ImageButton1;
+            this.guna2ImageButton1.Image = global::assetmanagementsystem.Properties.Resources.exitbutton1;
+            this.guna2ImageButton1.Location = new System.Drawing.Point(690, 10);
+            this.guna2ImageButton1.Name = "guna2ImageButton1";
+            this.guna2ImageButton1.PressedState.Parent = this.guna2ImageButton1;
+            this.guna2ImageButton1.Size = new System.Drawing.Size(50, 38);
+            this.guna2ImageButton1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(746, 494);
+            this.Controls.Add(this.guna2ImageButton1);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.gunaLabel3);
+            this.Controls.Add(this.tb_username);
+            this.Controls.Add(this.gunaLabel2);
+            this.Controls.Add(this.gunaLabel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Asset Management System Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI.WinForms.GunaLabel gunaLabel1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel2;
+        private Guna.UI2.WinForms.Guna2TextBox tb_username;
+        private Guna.UI.WinForms.GunaLabel gunaLabel3;
+        private Guna.UI2.WinForms.Guna2TextBox tb_password;
+        private Guna.UI2.WinForms.Guna2Button btn_login;
+        private Guna.UI2.WinForms.Guna2ImageButton guna2ImageButton1;
+    }
+}
+
---
+++ assetmanagementsystem/Form2.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace assetmanagementsystem
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.guna2Panel1.Location = new System.Drawing.Point(-3, -3);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.ShadowDecoration.Parent = this.guna2Panel1;
+            this.guna2Panel1.Size = new System.Drawing.Size(400, 976);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(1898, 968);
+            this.Controls.Add(this.guna2Panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Asset Management System";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+    }
+}
